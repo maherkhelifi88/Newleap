150f7fcd3a46fe8fb8a83b2547cd09d59e41c001
maherkhelifi88 <121508808+maherkhelifi88@users.noreply.github.com>
2024-02-12 15:17:43 +0100

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,10 +15,10 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                   UX Engineering Studio.
+                   AI UX Research boutique
                 </h1>
                 <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
-                Carefully refine, thoughtfully design, and meticulously test your innovative ideas. We are here to assist you every step of the way in building and bringing your creative visions to life.
+                Your UX Research partner in creating new AI products
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
